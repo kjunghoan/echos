85f4c3ef17834a4c8af03c01e34a1adcc102d492
Jung Hoan Kim <kjunghoan@gmail.com>
2023-08-23 02:58:13 -0400

    name update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,10 +3,10 @@ import { Menu, Typography } from "antd";
 import Link from "next/link";
 const { Title } = Typography;
 export default function NavBar() {
-  const appName: string = "Regional Messages(working title)"
+  const appName: string = "ECHOS(working title)"
   const menuListItems = [
     {
-      label: (<Typography.Link href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"><img width={70} src={"https://cdn.discordapp.com/attachments/1119419112486096957/1135677132362748004/Picsart_23-07-31_16-55-19-162.png"} /></Typography.Link>),
+      label: (<Typography.Link href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"><img width={70} src={""} /></Typography.Link>),
       key: 'theSong'
     },
     {
